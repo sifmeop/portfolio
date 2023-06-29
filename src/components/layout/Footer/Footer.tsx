c0bedc8db85439f810db0d06c93519bd0860cd5d
Eugene <sifmeop@gmail.com>
2023-06-29 12:54:13 +0300

    update link

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
         <a target='_blank' href='https://github.com/sifmeop'>
           <FaGithub size='2rem' />
         </a>
-        <a target='_blank' href=''>
+        <a target='_blank' href='https://www.linkedin.com/in/eugene-selivestru'>
           <FaLinkedin size='2rem' />
         </a>
       </div>
